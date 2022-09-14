3c206d65b7f25320a68f57061fd0a37e0b40b3cc
P-swa <pawarswatib777@gmail.com>
2022-09-14 14:58:53 +0530

    i am hungry

---
+++ SampleGit/src/github/Firstcommit.java
@@ -0,0 +1,17 @@
+package github;
+
+public class Firstcommit {
+
+	public static void main(String[] args) {
+		String Hungry="Yes";
+		if(Hungry=="Yes") 
+		{
+			System.out.println("i am hungry");
+		}
+		else 
+		{
+			System.out.println("committed");
+		}
+	}
+
+}
